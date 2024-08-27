89061ee6b5f6ff8d54808f89a97b3a1e8c936ce2
Afnan <mohammad.afnan@millsoft.com.pk>
2024-08-27 13:29:11 +0500

    27 aug 2024

--- netflix/practice.js
+++ netflix/practice.js
@@ -19,10 +19,17 @@ for (const key in obj) {
 for (const c of "Muhammad Afnan umer") {
     console.log(c);
 }
-// Array.forEach(element => {
-//     element.name,
-//     element.city,
-//     element.country,
-//     console.log(element)
 
-// });
+// i = 19;
+// while (i < 20) {
+//     console.log(i)
+//     i++;
+    
+// }
+
+j = 15;
+
+do {
+    console.log(j)
+    j++;  
+} while (j < 20);
