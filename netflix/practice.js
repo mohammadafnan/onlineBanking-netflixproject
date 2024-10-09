c07c2c7ae5fc47a792c51318c241dd6e777eb279
Afnan <mohammad.afnan@millsoft.com.pk>
2024-10-09 12:59:46 +0500

    9 oct 2024

--- netflix/practice.js
+++ netflix/practice.js
@@ -89,54 +89,222 @@ if (rand < 0.33) {
 
 console.log("Your business name" + ` ${first} ${second}  ${third} `);
 
-console.log("this is 1st");
-console.log("this is 2nd");
-setTimeout(() => {
-  console.log("this is 3rd ");
-}, 3000);
-console.log("this is 4th");
+// console.log("this is 1st");
+// console.log("this is 2nd");
+// setTimeout(() => {
+//   console.log("this is 3rd ");
+// }, 3000);
+// console.log("this is 4th");
 
-console.log("this is promise");
+// console.log("this is promise");
 
-let promise1 = new Promise((resolve, reject) => {
-  let time = Math.random();
-  if (time < 0.4) {
-    reject("Random no is less than 0.4 which is err");
-  } else {
-    setTimeout(() => {
-      // console.log("Promise one is done");
-      resolve("By promise 1");
-    }, 3000);
-  }
-});
-let promise2 = new Promise((resolve, reject) => {
-  let time = Math.random();
-  if (time < 0.4) {
-    reject("Random no is less than 0.4 which is err 2");
-  } else {
-    setTimeout(() => {
-      // console.log("Promise two is done");
-      resolve("By promise 2");
-    }, 5000);
-  }
-});
-let promise3 = new Promise((resolve, reject) => {
-  let time = Math.random();
-  if (time < 0.4) {
-    reject("Random no is less than 0.4 which is err 3");
-  } else {
-    setTimeout(() => {
-      // console.log("Promise three is done");
-      resolve("By promise 3");
-    }, 1000);
-  }
-});
+// let promise1 = new Promise((resolve, reject) => {
+//   let time = Math.random();
+//   if (time < 0.4) {
+//     reject("Random no is less than 0.4 which is err");
+//   } else {
+//     setTimeout(() => {
+//       resolve("By promise 1");
+//     }, 3000);
+//   }
+// });
+// let promise2 = new Promise((resolve, reject) => {
+//   let time = Math.random();
+//   if (time < 0.4) {
+//     reject("Random no is less than 0.4 which is err 2");
+//   } else {
+//     setTimeout(() => {
+//       resolve("By promise 2");
+//     }, 5000);
+//   }
+// });
+// let promise3 = new Promise((resolve, reject) => {
+//   let time = Math.random();
+//   if (time < 0.4) {
+//     reject("Random no is less than 0.4 which is err 3");
+//   } else {
+//     setTimeout(() => {
+//       resolve("By promise 3");
+//     }, 1000);
+//   }
+// });
 
-let promres = Promise.race([promise1, promise2, promise3]);
-promres
-  .then((x) => {
-    console.log(x);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// let promres = Promise.race([promise1, promise2, promise3]);
+// promres
+//   .then((x) => {
+//     console.log(x);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// async function getdata() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Promise Resolved");
+//     }, 3500);
+//   });
+// }
+
+// async function getdata() {
+//   let x = await fetch("https://jsonplaceholder.typicode.com/todos/101");
+//   let y = await x.text();
+//   console.log(y);
+// }
+
+// async function postdata() {
+//   let x = await fetch("https://jsonplaceholder.typicode.com/posts", {
+//     method: "POST",
+//     body: JSON.stringify({
+//       title: "Data 1",
+//       body: "ala ha",
+//     }),
+
+//     headers: {
+//       "Content-type": "application/json; charset=UTF-8",
+//     },
+//   });
+
+//   let postdata = await x.json();
+//   return postdata;
+// }
+
+// async function main() {
+//   console.log("loading Modules");
+
+//   console.log("loading data");
+
+//   console.log("Processing data");
+//   let data = await getdata();
+//   console.log(data);
+//   let pdata = await postdata();
+//   console.log(pdata);
+
+// data.then((v) => {
+//   console.log("Task 1");
+//   console.log("Task 2");
+//   console.log("Task 3");
+// });
+// console.log("Task 1");
+// console.log("Task 2");
+// console.log("Task 3");
+// }
+// main();
+
+// console.log("loading Modules");
+
+// console.log("loading data");
+
+// console.log("Processing data");
+// let data = getdata();
+// console.log(data);
+
+// console.log("Task 1");
+// console.log("Task 2");
+// console.log("Task 3");
+
+// let a = prompt("Enter first Number");
+// let b = prompt("Enter second Number");
+// let sum = parseInt(a) + parseInt(b);
+
+// if (isNaN(a) || isNaN(b)) {
+//   alert(SyntaxError("Only number is allowed"));
+// } else {
+//   try {
+//     alert("your sum is " + sum - x);
+//   } catch (error) {
+//     alert("bhai x nhi ha");
+//   } finally {
+//     alert("bhai bhand mar dya");
+//   }
+// }
+
+// let obj = {
+//   id: 10,
+//   name: "afnan",
+// };
+
+// console.log(obj);
+
+// let animal = {
+//   eats: true,
+// };
+
+// let rabbit = {
+//   jumps: true,
+// };
+
+// rabbit.__proto__ = animal;
+
+// class Animal {
+//   constructor(name) {
+//     this.name = name;
+//     console.log("Object is creating.....");
+//   }
+
+//   eats() {
+//     console.log("kha raha hoon");
+//   }
+
+//   jumps() {
+//     console.log("kood raha hoon");
+//   }
+// }
+
+// class Lion extends Animal {
+//   constructor(name) {
+//     super();
+//     this.name = name;
+//     console.log("Object is creating and he is lion.....");
+//   }
+
+//   eats() {
+//     super.eats();
+//     console.log("kha raha hoon or roar rha hoon");
+//   }
+// }
+
+// let a = new Animal("cat");
+// console.log(a);
+
+// let l = new Lion("sheer");
+// console.log(l);
+// "https://jsonplaceholder.typicode.com/todos/20"
+// "https://jsonplaceholder.typicode.com/posts"
+// headers: {
+//   "Content-type": "application/json; charset=UTF-8",
+// },
+
+// async function getData() {
+//   let x = await fetch("https://jsonplaceholder.typicode.com/todos");
+//   let y = await x.json();
+//   console.log(y);
+// }
+
+// async function postData() {
+//   let x = await fetch("https://jsonplaceholder.typicode.com/posts", {
+//     method: "POST",
+//     body: JSON.stringify({
+//       title: "Money heist all seasons",
+//       compeleted: true,
+//     }),
+
+//     headers: {
+//       "Content-type": "application/json; charset=UTF-8",
+//     },
+//   });
+//   let y = await x.json();
+//   console.log(y);
+// }
+
+// async function main() {
+//   console.log("Loading module...");
+//   console.log("Loading Data...");
+//   console.log("Loading ...");
+//   getData();
+//   postData();
+// }
+
+// main();
+
+
