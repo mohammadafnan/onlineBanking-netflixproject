0d2758506ee86281cbc7c5c4ba6d5d40fe7118e6
Afnan <mohammad.afnan@millsoft.com.pk>
2024-12-12 18:12:27 +0500

    12 dec 2024

--- netflix/test.js
+++ netflix/test.js
@@ -113,20 +113,59 @@ var u3 = new u2();
 // u3.name = "asd"
 console.log(u3.name);
 
+/////////////Discount Finder Code .... //////
 function checkdis() {
   var btn = document.getElementById("btn");
-
   var showdes = document.getElementById("desc");
-  var price = parseInt(document.getElementById("price").value);
-  var discountprice = parseInt(document.getElementById("discount").value);
+  var price = document.getElementById("price");
+  var discountprice = document.getElementById("discount");
+  var coupondiscount = document.getElementById("Coupon");
 
-  var discountfind = price - (price * discountprice) / 100;
-  /discount calc formular/;
-  showdes.textContent =
-    " You got " +
-    discountprice +
-    "% Discount " +
-    "and your Discounted price is " +
-    discountfind;
-  showdes.style.color = "green";
+  if (!price.value) {
+    showdes.textContent = "Please Add Price";
+    showdes.style.color = "white";
+    showdes.style.backgroundColor = "red";
+    showdes.style.marginTop = "10px";
+    showdes.style.padding = "10px";
+  } else if (coupondiscount.value == 2025) {
+    discountprice.value = 100;
+    var discountfind = price.value - (price.value * discountprice.value) / 100;
+    /discount calc formular/;
+    showdes.textContent =
+      "Current price is " +
+      price.value +
+      " and You got Special Discount (Coupon code)" +
+      discountprice.value +
+      "% Discount " +
+      "and your Discounted price is " +
+      discountfind;
+    showdes.style.color = "white";
+    showdes.style.backgroundColor = "#1545c9";
+    showdes.style.marginTop = "10px";
+    showdes.style.padding = "10px";
+  } else {
+    var discountfind = price.value - (price.value * discountprice.value) / 100;
+    /discount calc formular/;
+
+    showdes.textContent =
+      "Current price is " +
+      price.value +
+      " and You got " +
+      discountprice.value +
+      "% Discount " +
+      "and your Discounted price is " +
+      discountfind;
+
+    showdes.style.color = "white";
+    showdes.style.backgroundColor = "green";
+    showdes.style.marginTop = "10px";
+    showdes.style.padding = "10px";
+    price.value = 0;
+  }
 }
+
+// function reset() {
+//   var price = document.getElementById("price");
+
+//   price.value = 0;
+// }
